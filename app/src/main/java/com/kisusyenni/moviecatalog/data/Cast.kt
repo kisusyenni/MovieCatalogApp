956d316bc481a675110b3d5c6b557a7ce52accb5
Kisusyenni Venessa <kisusyenni01@gmail.com>
2021-11-24 04:04:58 +0700

    Delete unused files

--- app/src/main/java/com/kisusyenni/moviecatalog/data/Cast.kt
+++
@@ -1,7 +0,0 @@
-package com.kisusyenni.moviecatalog.data
-
-data class Cast (
-    var castName: String,
-    var roleName: String,
-    var castImage: String
-)
--- app/src/main/java/com/kisusyenni/moviecatalog/data/People.kt
+++
@@ -1,6 +0,0 @@
-package com.kisusyenni.moviecatalog.data
-
-data class People (
-    var name: String,
-    var position: String
-)
