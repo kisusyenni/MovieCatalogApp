35610cb0f42d3ed1eecc3e5ec225b1cc75a063e8
Kisusyenni Venessa <kisusyenni01@gmail.com>
2021-11-24 13:52:20 +0700

    Fix DetailViewModelTest.kt

--- app/src/test/java/com/kisusyenni/moviecatalog/ui/detail/DetailViewModelTest.kt
+++ app/src/test/java/com/kisusyenni/moviecatalog/ui/detail/DetailViewModelTest.kt
@@ -1,6 +1,7 @@
 package com.kisusyenni.moviecatalog.ui.detail
 
 import com.kisusyenni.moviecatalog.utils.MovieDummyData
+import com.kisusyenni.moviecatalog.utils.TvShowDummyData
 import junit.framework.TestCase.assertEquals
 import junit.framework.TestCase.assertNotNull
 import org.junit.Before
@@ -9,17 +10,18 @@ import org.junit.Test
 class DetailViewModelTest {
     private lateinit var viewModel: DetailViewModel
     private val dummyMovie = MovieDummyData.generateMovie()[0]
+    private val dummyTvShow = TvShowDummyData.generateTvShows()[0]
     private val movieId = dummyMovie.movieId
+    private val tvShowId = dummyTvShow.tvShowId
 
     @Before
     fun setUp() {
         viewModel = DetailViewModel()
-        viewModel.setSelectedDetail(movieId)
     }
 
     @Test
     fun getMovieDetail() {
-        viewModel.setSelectedDetail(dummyMovie.movieId)
+        viewModel.setSelectedDetail(movieId)
         val movieEntity = viewModel.getMovieDetail()
         assertNotNull(movieEntity)
         assertEquals(dummyMovie.movieId, movieEntity.movieId)
@@ -32,5 +34,22 @@ class DetailViewModelTest {
         assertEquals(dummyMovie.genres, movieEntity.genres)
         assertEquals(dummyMovie.releaseYear, movieEntity.releaseYear)
         assertEquals(dummyMovie.duration, movieEntity.duration)
+    }
+
+    @Test
+    fun getTvShowDetail() {
+        viewModel.setSelectedDetail(tvShowId)
+        val tvShowEntity = viewModel.getTvShowDetail()
+        assertNotNull(tvShowEntity)
+        assertEquals(dummyTvShow.tvShowId, tvShowEntity.tvShowId)
+        assertEquals(dummyTvShow.title, tvShowEntity.title)
+        assertEquals(dummyTvShow.overview, tvShowEntity.overview)
+        assertEquals(dummyTvShow.quote, tvShowEntity.quote)
+        assertEquals(dummyTvShow.image, tvShowEntity.image)
+        assertEquals(dummyTvShow.director, tvShowEntity.director)
+        assertEquals(dummyTvShow.rating, tvShowEntity.rating)
+        assertEquals(dummyTvShow.genres, tvShowEntity.genres)
+        assertEquals(dummyTvShow.releaseYear, tvShowEntity.releaseYear)
+        assertEquals(dummyTvShow.episodes, tvShowEntity.episodes)
     }
 }
