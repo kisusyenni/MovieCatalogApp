75a09d26957ae1b1d9fe6471e6ce3d7f012f1ef4
Kisusyenni Venessa <kisusyenni01@gmail.com>
2021-12-01 09:49:49 +0700

    Add files

---
+++ app/src/main/java/com/kisusyenni/moviecatalog/data/source/remote/RemoteDataSource.kt
@@ -0,0 +1,6 @@
+package com.kisusyenni.moviecatalog.data.source.remote
+
+import com.kisusyenni.moviecatalog.utils.JsonHelper
+
+class RemoteDataSource private constructor(private val jsonHelper: JsonHelper){
+}
---
+++ app/src/main/java/com/kisusyenni/moviecatalog/utils/JsonHelper.kt
@@ -0,0 +1,23 @@
+package com.kisusyenni.moviecatalog.utils
+
+import android.content.Context
+import java.io.IOException
+
+class JsonHelper(private val context: Context) {
+
+    private fun parsingFileToString(fileName: String): String? {
+        return try {
+            val `is` = context.assets.open(fileName)
+            val buffer = ByteArray(`is`.available())
+            `is`.read(buffer)
+            `is`.close()
+            String(buffer)
+
+        } catch (ex: IOException) {
+            ex.printStackTrace()
+            null
+        }
+    }
+
+
+}
