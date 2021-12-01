8ffebcfa8177aede844d49cd947cd004cb9931ca
Kisusyenni Venessa <kisusyenni01@gmail.com>
2021-12-01 09:48:50 +0700

    Add get tv show list and detail

--- app/src/main/java/com/kisusyenni/moviecatalog/data/source/remote/api/ApiService.kt
+++ app/src/main/java/com/kisusyenni/moviecatalog/data/source/remote/api/ApiService.kt
@@ -2,6 +2,8 @@ package com.kisusyenni.moviecatalog.data.source.remote.api
 
 import com.kisusyenni.moviecatalog.data.source.remote.response.MovieDetailResponse
 import com.kisusyenni.moviecatalog.data.source.remote.response.MovieResponse
+import com.kisusyenni.moviecatalog.data.source.remote.response.TvShowDetailResponse
+import com.kisusyenni.moviecatalog.data.source.remote.response.TvShowResponse
 import retrofit2.Call
 import retrofit2.http.*
 
@@ -15,4 +17,14 @@ interface ApiService {
     fun getMovieDetail(
         @Path("movie_id") movie_id: String
     ): Call<MovieDetailResponse>
+
+    @GET("tv/popular")
+    fun getTvShows(
+        @Query("page") page:Int
+    ): Call<TvShowResponse>
+
+    @GET("tv/{tv_id}")
+    fun getTvShowDetail(
+        @Path("tv_id") tv_id: String
+    ): Call<TvShowDetailResponse>
 }
