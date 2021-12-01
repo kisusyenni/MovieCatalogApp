1421d9db998e2336e3504962b38e5a1583cca62f
Kisusyenni Venessa <kisusyenni01@gmail.com>
2021-12-01 09:49:37 +0700

    Using livedata and api to movie list

--- app/src/main/java/com/kisusyenni/moviecatalog/ui/movie/MovieListAdapter.kt
+++ app/src/main/java/com/kisusyenni/moviecatalog/ui/movie/MovieListAdapter.kt
@@ -7,14 +7,16 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.kisusyenni.moviecatalog.R
 import com.kisusyenni.moviecatalog.data.MovieEntity
+import com.kisusyenni.moviecatalog.data.source.remote.api.POSTER_BASE_URL
+import com.kisusyenni.moviecatalog.data.source.remote.response.MovieResultsItem
 import com.kisusyenni.moviecatalog.databinding.ItemsMovieBinding
 import com.kisusyenni.moviecatalog.ui.detail.DetailActivity
 
 class MovieListAdapter: RecyclerView.Adapter<MovieListAdapter.MovieViewHolder>() {
 
-    private var listMovies = ArrayList<MovieEntity>()
+    private var listMovies = ArrayList<MovieResultsItem>()
 
-    fun setMovies(movies: List<MovieEntity>?) {
+    fun setMovies(movies: List<MovieResultsItem>?) {
         if (movies == null) return
         this.listMovies.clear()
         this.listMovies.addAll(movies)
@@ -33,19 +35,22 @@ class MovieListAdapter: RecyclerView.Adapter<MovieListAdapter.MovieViewHolder>()
     override fun getItemCount(): Int = listMovies.size
 
     class MovieViewHolder(private val binding: ItemsMovieBinding) : RecyclerView.ViewHolder(binding.root) {
-        fun bind(movie: MovieEntity) {
+        fun bind(movie: MovieResultsItem) {
             with(binding) {
                 tvItemTitle.text = movie.title
-                tvItemGenres.text = itemView.resources.getString(R.string.genres, movie.genres)
-                rbItemRating.rating = (movie.rating/20).toFloat()
+                tvItemReleased.text = movie.releaseDate
+                movie.voteAverage?.let {
+                    rbItemRating.rating = (it/2).toFloat()
+                }
                 itemView.setOnClickListener {
                     val intent = Intent(itemView.context, DetailActivity::class.java)
-                    intent.putExtra(DetailActivity.EXTRA_ID, movie.movieId)
+                    intent.putExtra(DetailActivity.EXTRA_ID, movie.id)
                     intent.putExtra(DetailActivity.EXTRA_CATEGORY, 1)
                     itemView.context.startActivity(intent)
                 }
+                val posterUrl = POSTER_BASE_URL + movie.posterPath
                 Glide.with(itemView.context)
-                    .load(movie.image)
+                    .load(posterUrl)
                     .into(imgPoster)
             }
         }
--- app/src/main/java/com/kisusyenni/moviecatalog/ui/movie/MovieListFragment.kt
+++ app/src/main/java/com/kisusyenni/moviecatalog/ui/movie/MovieListFragment.kt
@@ -1,11 +1,13 @@
 package com.kisusyenni.moviecatalog.ui.movie
 
+import android.content.res.Configuration
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.kisusyenni.moviecatalog.databinding.FragmentMovieListBinding
 
@@ -13,24 +15,44 @@ class MovieListFragment : Fragment() {
 
     private lateinit var fragmentMovieListBinding: FragmentMovieListBinding
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        fragmentMovieListBinding = FragmentMovieListBinding.inflate(layoutInflater, container, false)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        fragmentMovieListBinding =
+            FragmentMovieListBinding.inflate(layoutInflater, container, false)
         return fragmentMovieListBinding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         if (activity != null) {
-            val viewModel = ViewModelProvider(this, ViewModelProvider.NewInstanceFactory())[MovieListViewModel::class.java]
-            val movies = viewModel.getMovies()
-
+            val viewModel = ViewModelProvider(
+                this,
+                ViewModelProvider.NewInstanceFactory()
+            )[MovieListViewModel::class.java]
             val movieAdapter = MovieListAdapter()
-            movieAdapter.setMovies(movies)
-            with(fragmentMovieListBinding.rvMovieList) {
-                layoutManager = LinearLayoutManager(context)
-                setHasFixedSize(true)
-                adapter = movieAdapter
-            }
+
+            viewModel.movieList.observe(viewLifecycleOwner, { movies ->
+
+                movieAdapter.setMovies(movies)
+                with(fragmentMovieListBinding.rvMovieList) {
+                    if (resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                        layoutManager = GridLayoutManager(context, 4)
+                    } else {
+                        layoutManager = GridLayoutManager(context, 3)
+                    }
+                    setHasFixedSize(true)
+                    adapter = movieAdapter
+                }
+
+            })
+            viewModel.isLoading.observe(viewLifecycleOwner, {
+                fragmentMovieListBinding.progressBar.visibility =
+                    if (it) View.VISIBLE else View.GONE
+            })
+
         }
     }
 }
